52b1490ea401dd0daba0788a64262eacd1a5e24e
Sengottayan <sengosaminathan@gmail.com>
2026-04-20 10:13:15 +0530

    otp-verification-v.2

--- server/src/models/Candidate.js
+++ server/src/models/Candidate.js
@@ -35,9 +35,9 @@ candidateSchema.methods.matchPassword = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password);
 };
 
-candidateSchema.pre('save', async function (next) {
+candidateSchema.pre('save', async function () {
     if (!this.isModified('password')) {
-        next();
+        return;
     }
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
--- server/src/models/Recruiter.js
+++ server/src/models/Recruiter.js
@@ -39,9 +39,9 @@ recruiterSchema.methods.matchPassword = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password);
 };
 
-recruiterSchema.pre('save', async function (next) {
+recruiterSchema.pre('save', async function () {
     if (!this.isModified('password')) {
-        next();
+        return;
     }
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
